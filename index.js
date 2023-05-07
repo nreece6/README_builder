dbf3d12c26f1c1089d3ea2ed1efcfca46f1e8803
nreece6 <reece.nick24@gmail.com>
2023-05-07 12:59:37 -0500

    built out question template and basic readme generator function

--- index.js
+++ index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+{
 
-// TODO: Create a function to initialize app
-function init() {}
+type: 'input',
+name: 'title',
+message: 'What is the title of your project?',
 
-// Function call to initialize app
-init();
+},
+
+{
+
+type: 'input',
+name: 'description',
+message: 'Enter a description of your project.',
+
+},
+
+{
+
+type: 'input',
+name: 'install',
+message: 'Enter installation instructions for your application.',
+
+},
+
+{
+
+type: 'input',
+name: 'usage',
+message: 'Enter relevant usage information.',
+
+},
+
+{
+
+type: 'input',
+name: 'contribution',
+message: 'Enter contribution guidelines',
+
+},
+
+{
+
+type: 'input',
+name: 'tests',
+message: 'Enter any test instructions.',
+
+},
+
+{
+
+type: 'input',
+name: 'license',
+message: 'Enter your LinkedIn URL.',
+
+},
+
+{
+
+type: 'input',
+name: 'github',
+message: 'Enter your GitHub account.',
+        
+},
+
+{
+
+type: 'input',
+name: 'email',
+message: 'Enter your email address.',
+    
+},
+
+])
+
+.then((answers) => {
+
+const readMePage = generateMarkdown(answers);
+
+fs.writeFile('README.md', readMePage, (err) =>
+
+err ? console.log(err) : console.log('Successfully created index.html!')
+
+);
+
+});
